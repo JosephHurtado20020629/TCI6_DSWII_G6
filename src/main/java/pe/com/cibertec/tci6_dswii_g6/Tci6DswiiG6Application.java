168dc2a76273a14580e3b4e3022f9d0c5af5eaf2
Joseph Hurtado <joseph.hurtado.magan@gmail.com>
2022-11-28 19:47:54 -0500

    Initial commit

---
+++ src/main/java/pe/com/cibertec/tci6_dswii_g6/Tci6DswiiG6Application.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.tci6_dswii_g6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tci6DswiiG6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tci6DswiiG6Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/cibertec/tci6_dswii_g6/Tci6DswiiG6ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.tci6_dswii_g6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tci6DswiiG6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
